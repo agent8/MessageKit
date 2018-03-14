72ad21a923b83fe7c45f064480cb2ef3fc562143
Clarence Chee <clarencechee94@gmail.com>
2018-03-14 11:24:15 -0700

    darkgray

--- Sources/Views/MessageInputBar.swift
+++ Sources/Views/MessageInputBar.swift
@@ -130,7 +130,7 @@ open class MessageInputBar: UIView {
             .onEnabled {
                 $0.imageView?.tintColor = nil
             }.onDisabled {
-                $0.imageView?.tintColor = .darkGray
+                $0.imageView?.tintColor = COLOR_TINT_DARK_GRAY
             }.configure {
                 $0.setSize(CGSize(width: 52, height: 28), animated: false)
                 $0.isEnabled = false
