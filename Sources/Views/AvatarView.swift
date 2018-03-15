e0c4e2d941b545352484e29b9e6d14d310fe8e5d
Tan Wei <tanwei0319@gmail.com>
2018-03-14 22:57:23 -0700

    AvatarView: set default background color to clear

--- Sources/Views/AvatarView.swift
+++ Sources/Views/AvatarView.swift
@@ -157,7 +157,7 @@ open class AvatarView: UIImageView {
     // MARK: - Internal methods
 
     internal func prepareView() {
-        backgroundColor = .gray
+        backgroundColor = .clear
         contentMode = .scaleAspectFill
         layer.masksToBounds = true
         clipsToBounds = true
